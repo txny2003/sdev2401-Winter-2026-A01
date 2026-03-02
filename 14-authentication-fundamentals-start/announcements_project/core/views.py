7b50d803834b9a751e083bd73dfa7754f0dafb58
Daniel Mouris <dgmouris@gmail.com>
2026-03-02 11:31:08 -0700

    class progress pushed

--- 14-authentication-fundamentals-start/announcements_project/core/views.py
+++ 14-authentication-fundamentals-start/announcements_project/core/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+# import our new form here
+from .forms import UserRegistrationForm
+
+# let's create the view to register a user
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save() # save the new user to the database
+            login(request, user)
+
+            # redirect the user on successful form submission to
+            # the annoucements list
+            return redirect("announcements_list")
+            # where "announcements_list" is the name of the url
+            # to redirect to.
+
+    form = UserRegistrationForm()
+    return render(
+        request,
+        'core/register.html',
+        { "form": form }
+    )
