bb516beb2c663445295de3b21346da9eabef0daa
Daniel Mouris <dgmouris@gmail.com>
2026-03-05 10:50:02 -0700

    class progress pushed

--- 14-authentication-fundamentals-start/announcements_project/core/urls.py
+++ 14-authentication-fundamentals-start/announcements_project/core/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     # more on this later
     path("login/", custom_login, name="login"),
     # we're also going to add the logout view which is a form.
-    path("logout/", LogoutView.as_view(), name="logout")
+    path("logout/", LogoutView.as_view(),
+         name="logout")
 ]
 
 # take a look at the login settings in settings.py to see
