3082ba332040f576bc4ed54de2e5d4a00398b09d
Daniel Mouris <dgmouris@gmail.com>
2026-03-06 13:37:16 -0700

    class progress pushed

--- 15-user-specific-data-start/announcements_project/announcements/views.py
+++ 15-user-specific-data-start/announcements_project/announcements/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 # import login_required decorator
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import (
+    login_required, user_passes_test)
+
+# import the permission from core.
+from core.permissions import is_teacher
 
 # Create your views here.
 from .models import Announcement
@@ -17,7 +21,9 @@ def announcement_list(request):
 
 
 @login_required
+@user_passes_test(is_teacher)
 def create_announcement(request):
+    # request.user is on every request.
     # only teachers can create announcements
     # students cannot.
     return render(request, 'announcements/create_announcement.html', {})
