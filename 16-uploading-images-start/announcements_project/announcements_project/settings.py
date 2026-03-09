60f2a7d65801e6bf7ff09419dcb4e71812744130
Daniel Mouris <dgmouris@gmail.com>
2026-03-09 10:41:51 -0600

    class progress pushed

--- 16-uploading-images-start/announcements_project/announcements_project/settings.py
+++ 16-uploading-images-start/announcements_project/announcements_project/settings.py
@@ -118,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -133,3 +137,4 @@ AUTH_USER_MODEL = 'core.User'
 LOGIN_REDIRECT_URL = '/announcements/'  # after login original LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'  # after logout
 LOGIN_URL = '/accounts/login/' #
+
