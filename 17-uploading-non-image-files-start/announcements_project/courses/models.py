82cc6c847f196e41624f1ea945e5e22fb7eb67d1
Daniel Mouris <dgmouris@gmail.com>
2026-03-12 11:28:49 -0600

    class progress pushed

--- 17-uploading-non-image-files-start/announcements_project/courses/models.py
+++ 17-uploading-non-image-files-start/announcements_project/courses/models.py
@@ -18,3 +18,23 @@ class Assignment(models.Model):
 
 
 # create a submission model
+# - assignment: foreign key (use cascade)
+# - student: name
+# - file: filefield (upload to "submissions/")
+# - subitted_at: DateTimeField when its' created.
+class Submission(models.Model):
+    assignment = models.ForeignKey(
+        Assignment,
+        on_delete=models.CASCADE,
+        related_name="submissions",
+    )
+    student_name = models.CharField(max_length=100)
+
+    file = models.FileField(upload_to="submissions/")
+    # inside of media this will uplaod to "submissions/"
+    # we need to confirm we have the MEDIA_ROOT and MEDIA_URL
+
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Submission by {self.student_name} for {self.assignment}"
