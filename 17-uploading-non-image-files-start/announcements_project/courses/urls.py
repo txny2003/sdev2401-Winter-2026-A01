16f55b40b1c29ff4cb5626d611ae3fd738ca360c
Daniel Mouris <dgmouris@gmail.com>
2026-03-13 13:46:51 -0600

    class progress pushed

---
+++ 17-uploading-non-image-files-start/announcements_project/courses/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import bulk_assignment_upload
+
+urlpatterns = [
+    path(
+        "bulk-assignment-upload",
+        bulk_assignment_upload,
+        name="bulk_assignment_upload",
+    ),
+]
