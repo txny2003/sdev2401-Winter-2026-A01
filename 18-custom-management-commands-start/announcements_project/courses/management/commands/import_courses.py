34dccd6b744a9926fe1d62b1d02ba4ed84870e56
Daniel Mouris <dgmouris@gmail.com>
2026-03-16 10:47:20 -0600

    class progress pushed

--- 18-custom-management-commands-start/announcements_project/courses/management/commands/import_courses.py
+++ 18-custom-management-commands-start/announcements_project/courses/management/commands/import_courses.py
@@ -15,5 +15,18 @@ class Command(BaseCommand):
     # add an argument for the path of the csv file.
     def add_argument(self, parser):
         parser.add_argument(
-            "csv_file",  #
+            "csv_file",  # arguemnt name
+            type=str,  # this will be a string
+            help="Path to file for import",
         )
+
+    # *args and **kwargs, you'll see this a lot this means
+    # args (which are positinal arguments) are a list in the
+    # function
+    # kwargs (named arguments) are a dictionary of named
+    # items.
+    # this makes it so that you can pass in a flexible
+    # number of arguments.
+    # Note: the function below is what will be executed
+    def handle(self, *args, **kwargs):
+        print("Hello from my command!")
