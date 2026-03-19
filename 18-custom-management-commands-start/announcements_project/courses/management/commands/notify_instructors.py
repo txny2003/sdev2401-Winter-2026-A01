89b6afcb61add0d6122c4f67b79e4775e6077027
Daniel Mouris <dgmouris@gmail.com>
2026-03-19 10:31:57 -0600

    class progress pushed

---
+++ 18-custom-management-commands-start/announcements_project/courses/management/commands/notify_instructors.py
@@ -0,0 +1,9 @@
+# to create a command
+# with no arguments
+# will get all submissions that the instructor
+# has not been notified.
+# count submissions
+# loop through the submissions
+# send an email
+# update each submission model
+# say that the instructor has been notified.
