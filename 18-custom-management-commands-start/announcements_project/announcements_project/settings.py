2f8d384beae47629a2d942428f536ee9a47189f2
Daniel Mouris <dgmouris@gmail.com>
2026-03-19 10:48:53 -0600

    class progress pushed

--- 18-custom-management-commands-start/announcements_project/announcements_project/settings.py
+++ 18-custom-management-commands-start/announcements_project/announcements_project/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
     # custom apps
     "core",
     "announcements",
@@ -61,7 +60,7 @@ ROOT_URLCONF = "announcements_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ['templates'],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -118,8 +117,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -127,23 +124,23 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # you can add this at the bottom of the file
 # as it's not created yet.
-AUTH_USER_MODEL = 'core.User'
+AUTH_USER_MODEL = "core.User"
 
 
-LOGIN_REDIRECT_URL = '/announcements/'  # after login original LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/accounts/login/'  # after logout
-LOGIN_URL = '/accounts/login/' #
+LOGIN_REDIRECT_URL = "/announcements/"  # after login original LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = "/accounts/login/"  # after logout
+LOGIN_URL = "/accounts/login/"  #
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 # Static files
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
+# email settings
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
--- 18-custom-management-commands-start/announcements_project/courses/management/commands/notify_instructors.py
+++ 18-custom-management-commands-start/announcements_project/courses/management/commands/notify_instructors.py
@@ -1,9 +1,35 @@
+from django.core.management.base import BaseCommand
+from django.core.mail import send_mail
+
+from courses.models import Submission
+
+
 # to create a command
 # with no arguments
-# will get all submissions that the instructor
-# has not been notified.
-# count submissions
-# loop through the submissions
-# send an email
-# update each submission model
-# say that the instructor has been notified.
+class Command(BaseCommand):
+    help = "Notifies instructors of new submissions"
+
+    def handle(self, *args, **kwargs):
+        # will get all submissions that the instructor
+        # has not been notified.
+        new_submissions = Submission.objects.filter(
+            instructor_notified=False,
+        )
+        # count submissions
+        count = new_submissions.count()
+        # loop through the submissions
+        for submission in new_submissions:
+            to_email = submission.assignment.owner.email
+            # send an email
+            send_mail(
+                subject=f"New submssion from {submission.student_name} for {submission.assignment.title}",
+                message=f"""
+                {submission.assignment.title} has a new submission from {submission.student_name}
+
+                Download it here: {submission.file.url}
+                """,
+                from_email="notifications@do-not-reply.com",  # your admin email.
+                recipient_list=[to_email],
+            )
+            # update each submission model
+            # say that the instructor has been notified.
