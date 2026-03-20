30ae5916ce05023f26f8946960ecc23275bd1805
Daniel Mouris <dgmouris@gmail.com>
2026-03-20 13:39:59 -0600

    class progress pushed

--- 19-class-based-views-start/announcements_project/announcements/views.py
+++ 19-class-based-views-start/announcements_project/announcements/views.py
@@ -53,6 +53,37 @@ def announcement_list(request):
 # refer to the slides if you're confused.
 
 
+class CreateAnnouncementView(View):
+    template_name = "announcements/create_announcement.html"
+    form_class = AnnouncementForm
+
+    # handling the request.method of get
+    def get(self, request):
+        form = self.form_class()
+        return render(
+            request,
+            self.template_name,
+            {"form": form},
+        )
+
+    # handling the request.method of post
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            announcement = form.save(commit=False)
+            # the commit false will prevent the form from saving to the database
+            # set the created_by field to the current user
+            announcement.created_by = request.user
+            announcement.save()
+            # save the announcement to the database.
+            return redirect("announcement_list")
+        return render(
+            request,
+            self.template_name,
+            {"form": form},
+        )
+
+
 # this will restrict access to only users that pass the is_teacher test
 # it will redirect to the login page if the user does not have permission.
 @login_required
